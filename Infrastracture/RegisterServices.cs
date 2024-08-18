d6db21f539c084106152f049ae2df5ffa10889f7
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-08-18 10:49:52 +0500

    DATA: Create Infrastructure Class Type Project

---
+++ Infrastracture/RegisterServices.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastracture
+{
+    public static class RegisterServices
+    {
+        public static IServiceCollection AddInfrastructureServices(
+            this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
