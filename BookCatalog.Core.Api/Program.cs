ab926cc1ecc8a27303c1a01df49e414c0d72fe15
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-08-18 10:26:42 +0500

    CODERUB: Remov Uneccessary Files And Comments

--- BookCatalog.Core.Api/Program.cs
+++ BookCatalog.Core.Api/Program.cs
@@ -7,16 +7,12 @@ namespace BookCatalog.Core.Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -26,7 +22,6 @@ namespace BookCatalog.Core.Api
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
