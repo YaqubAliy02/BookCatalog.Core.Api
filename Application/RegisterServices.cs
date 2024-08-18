ccc4bb39bc116a88041d5c9d58af607678f6608c
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-08-18 10:47:19 +0500

    DATA: Add Application Class Type Project

---
+++ Application/RegisterServices.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class RegisterServices
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
