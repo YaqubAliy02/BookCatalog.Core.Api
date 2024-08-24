5a9b07f8820c186aef6bbc16d85ddb9e997abfdb
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-08-24 11:28:54 +0500

    Check Token

--- Infrastracture/RegisterServices.cs
+++ Infrastracture/RegisterServices.cs
@@ -1,10 +1,14 @@
-﻿using Application.Abstraction;
+﻿using System.Security.Cryptography;
+using System.Text;
+using Application.Abstraction;
 using Application.Repositories;
 using Infrastracture.Persistence;
 using Infrastracture.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Infrastracture
 {
@@ -19,6 +23,36 @@ namespace Infrastracture
             services.AddScoped<IBookRepository, BookRepository>();
             services.AddScoped<IAuthorRepository, AuthorRepository>();
             services.AddScoped<IUserRepository, UserRepository>();
+
+            //*************** This is for checking user's token that is it valid and invalid and reading it.
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidAudience = configuration["JWT:AudienceKey"],
+                        ValidIssuer = configuration["JWT:IssuerKey"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Key"])),
+                        ClockSkew = TimeSpan.Zero
+                    };
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnAuthenticationFailed = (context) =>
+                        {
+                            if(context.Exception.GetType()  == typeof(SecurityTokenExpiredException))
+                            {
+                                context.Response.Headers.Add("IsTokenExpired", "true");
+                            }
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+            //*******************************************************************************************
             return services;
         }
     }
