4bb4f6ac5a061f50b96854f091a6fa8c5b6a9807
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-08-28 17:20:03 +0500

    FILTERS: Add Exception Filter

---
+++ BookCatalog.Core.Api/Filters/CustormExceptionFilterAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BookCatalog.Core.Api.Filters
+{
+    public class CustormExceptionFilterAttribute : Attribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var result = new ObjectResult("An error occured. This is Custom Exception Filter error handler")
+            {
+                StatusCode = StatusCodes.Status500InternalServerError,
+            };
+            context.Result = result;
+        }
+    }
+}
