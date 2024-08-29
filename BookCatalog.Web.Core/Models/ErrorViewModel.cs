04ecc6e49930df6ad05c44af356df100ca1d15a9
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-08-29 10:47:43 +0500

    INFRA: Create MVC Project

---
+++ BookCatalog.Web.Core/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookCatalog.Web.Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
