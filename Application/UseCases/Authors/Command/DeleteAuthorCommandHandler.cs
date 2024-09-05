1cf1084aa7386e8dd786c1d3306988d484bad71d
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-09-05 22:29:54 +0500

    CONTROLLERS: Change DeleteAuthor to MediatR

---
+++ Application/UseCases/Authors/Command/DeleteAuthorCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Application.Repositories;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.UseCases.Authors.Command
+{
+    public class DeleteAuthorCommand : IRequest<IActionResult>
+    {
+        public Guid Id { get; set; }
+    }
+    public class DeleteAuthorCommandHandler : IRequestHandler<DeleteAuthorCommand, IActionResult>
+    {
+        private readonly IAuthorRepository _authorRepository;
+
+        public DeleteAuthorCommandHandler(IAuthorRepository authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+
+        public async Task<IActionResult> Handle(DeleteAuthorCommand request, CancellationToken cancellationToken)
+        {
+            bool isDelete = await _authorRepository.DeleteAsync(request.Id);
+
+            if (isDelete) return new OkObjectResult("Author is deleted successfully");
+
+            return new BadRequestObjectResult("Delete operation has been failed");
+        }
+    }
+}
