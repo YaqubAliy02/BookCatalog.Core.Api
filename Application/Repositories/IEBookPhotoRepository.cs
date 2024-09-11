1f07c25a5a66763312d370836575367e58c6fb8c
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-09-11 09:27:33 +0500

    CODERUB: Remove Unnecessary Files

--- Application/Repositories/IEBookPhotoRepository.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Application.Repositories
-{
-    public interface IEBookPhotoRepository
-    {
-        Task<string> AddEBookPhotoAsync(Stream fileStream, string fileName, string contentType);
-    }
-}
--- Infrastracture/External/IBlobStorage.EBookPhoto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Infrastracture.External
-{
-    public partial interface IBlobStorage
-    {
-        Task<string> UploadEBookPhotoAsync(Stream fileStream, string fileName, string contentType);
-    }
-}
