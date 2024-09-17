cc21e6d66d5d4f4bd61a254111e17ace378f9c98
Yaqubaliy Majamolov <91617731+YaqubAliy02@users.noreply.github.com>
2024-09-17 19:27:04 +0500

    Delete BookCatalog.Web.Core directory

--- BookCatalog.Web.Core/Models/IndexStartModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookCatalog.Web.Core.Models
-{
-    public class IndexStartModel
-    {
-        public int StatusCode { get; set; }
-        public object ErrorMessage { get; set; }
-        public object SuccessMessage { get; set; }
-    }
-}
