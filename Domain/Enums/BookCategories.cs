aad848bf3b5c9fdfcbb52880a0fb76b01bb6f195
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-09-19 20:17:02 +0500

    FEATURES: Add Delete Feature With Confirmation Alert

--- Domain/Enums/BookCategories.cs
+++ Domain/Enums/BookCategories.cs
@@ -8,5 +8,12 @@ namespace Domain.Enums
         Religious = 2,
         Fantastic = 3,
         Historical = 4,
+        Business = 5,
+        Programming = 6,
+        Brain = 7,
+        Self_Development = 8,
+        Psychology = 9,
+        Soft_Skill = 10,
+        IELTS = 11,
     }
 }
