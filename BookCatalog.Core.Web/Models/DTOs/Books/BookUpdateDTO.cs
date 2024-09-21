0253f7568bacad96f15a1a371bc77c751ab1100a
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-09-21 09:03:48 +0500

    DATA: Add UpdateBook Page And BookUpdateDTO Model But Don't Use

---
+++ BookCatalog.Core.Web/Models/DTOs/Books/BookUpdateDTO.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Enums;
+
+namespace BookCatalog.Core.Web.Models.DTOs.Books
+{
+    public class BookUpdateDTO
+    {
+        public string Name { get; set; }
+        public string ISBN { get; set; }
+        public string Description { get; set; }
+        public string BlobPath { get; set; }
+        public string Thubnail { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public Guid[] AuthorsId { get; set; }
+        public byte Categories { get; set; }
+    }
+}
